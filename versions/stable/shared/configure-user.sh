a33e36e0109796b66334d45210343ef4d397c386
Stepan Koci <stepan.koci@imatic.cz>
2016-11-20 23:28:58 +0100

    Gitconfig added

--- versions/stable/shared/configure-user.sh
+++ versions/stable/shared/configure-user.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# Git
-git config --global core.editor vim
-git config --global color.ui true
-git config --global core.fileMode false
-
-# Vim
+cp /resources/debian/shared/.gitconfig ~/
 cp /resources/debian/shared/.vimrc ~/
